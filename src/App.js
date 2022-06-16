8b51932680e92b9f3f75e03c35e4207d5aa7b246
Adrián Ferreres Esteller <ardiadrianadri@gmail.com>
2022-06-16 19:03:14 +0200

    add theme and storybook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
+import TestComponents from './test-components/test-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <h1>This is the home app</h1>
+       <Link to="/test">To test</Link>
+       <TestComponents />
     </div>
   );
 }
