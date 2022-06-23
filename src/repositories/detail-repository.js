370408ae7bf8f65783765e6ef68b3f32ed264e54
Adrián Ferreres Esteller <ardiadrianadri@gmail.com>
2022-06-23 22:36:13 +0200

    finish detail-repository

--- src/repositories/detail-repository.js
+++ src/repositories/detail-repository.js
@@ -20,10 +20,51 @@ const getPictures = {
   [STARSHIPS]: getStarshipsPicture
 }
 
+const dataProperties = {
+  [CHARACTERS]: ['name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year', 'gender'],
+  [PLANETS]: ['name','rotation_period','orbital_period','diameter','climate','gravity','terrain','surface_water','population'],
+  [STARSHIPS]: ['name','model','manufacturer','cost_in_credits','length','max_atmosphering_speed','crew','passengers','cargo_capacity','consumables','hyperdrive_rating','MGLT','starship_class']
+}
+
+const planetsProperties = {
+  [CHARACTERS]: 'homeworld'
+}
+const charactersProperties = {
+  [PLANETS]: 'residents',
+  [STARSHIPS]: 'pilots'
+};
+const starshipsProperties = {
+  [CHARACTERS]: 'starships'
+};
+
 class DetailRepository {
 
-  _successManager(data) {
+  _proccessRelatedData(data) {
+    if (!Array.isArray(data)) {
+      return [data];
+    }
+
     return data;
+  }
+
+  _successManager(type) {
+    return (data) => {
+      const elementData = dataProperties[type]
+        .reduce((acc, property) => ({
+          ...acc,
+          [property.replace(/_/g, ' ')]: data[property]
+        }), {});
+
+      elementData.picture = getPictures[type](data.name);
+
+      const related = {
+        [CHARACTERS]: (charactersProperties[type]) ? this._proccessRelatedData(data[charactersProperties[type]]) : null,
+        [PLANETS]: (planetsProperties[type]) ? this._proccessRelatedData(data[planetsProperties[type]]) : null,
+        [STARSHIPS]: (starshipsProperties[type]) ? this._proccessRelatedData(data[starshipsProperties[type]]) :  null
+      }
+
+      return { elementData, related };
+    }
   }
 
   _errorManager(type) {
@@ -57,11 +98,11 @@ class DetailRepository {
         request = swapiDataSource.getPlanetInfo(id);
       break;
       default:
-        request = swapiDataSource.getStarshipInfo(id);
+        request = swapiDataSource.getStarshipsInfo(id);
     }
 
     return request
-      .then(this._successManager)
+      .then(this._successManager(type))
       .catch(this._errorManager(type));
   }
 }
